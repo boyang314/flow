3406da3f03c5d4dbcb852506d50df34b7fcad49f
bliu <boyangliu@yahoo.com>
2021-03-13 10:43:30 -0500

    add impl detail

--- net/epoll/message/messaging/GenericMessage.C
+++ net/epoll/message/messaging/GenericMessage.C
@@ -1,8 +1,27 @@
 #include "GenericMessage.H"
 
 void GenericMessage::fromMessageBuffer(MessageInputBuffer& inputBuffer) {
+    while(inputBuffer.hasMoreFields()) {
+        MessageFieldInfo fieldInfo = inputBuffer.nextFieldInfo();
+        switch (fieldInfo.getFieldType().type()) {
+        case MessageFieldTypeEnum::_int: set<int>(fieldInfo.getFieldId(), inputBuffer.get<int>()); break;
+        case MessageFieldTypeEnum::_double: set<double>(fieldInfo.getFieldId(), inputBuffer.get<double>()); break;
+        case MessageFieldTypeEnum::_string: setString(fieldInfo.getFieldId(), inputBuffer.getString(fieldInfo.getFieldLength()), fieldInfo.getFieldLength()); break;
+        default: inputBuffer.skipField(fieldInfo); break;
+        }
+    }
 }
 
 void GenericMessage::toMessageBuffer(MessageOutputBuffer& outputBuffer, const uint16_t messageType) const {
+    outputBuffer.setup(messageType, fields_.size());
+    for (const auto& field : fields_) {
+        switch(field.getFieldType().type()) {
+        case MessageFieldTypeEnum::_int: break;
+        case MessageFieldTypeEnum::_double: break;
+        case MessageFieldTypeEnum::_string: break;
+        default: break;
+        }
+    }
+    outputBuffer.commit();
 }
 
--- net/epoll/message/messaging/test.C
+++ net/epoll/message/messaging/test.C
@@ -1,9 +1,29 @@
 #include "test.H"
 #include <iostream>
 
+void describeFieldInfo(const MessageFieldInfo& fieldInfo) {
+    std::cout << "fieldId: " << fieldInfo.getFieldId() << " fieldType: " << (int)fieldInfo.getFieldType().type() << " len: " << fieldInfo.getFieldLength() << std::endl;
+}
+
+void describeMessageHeader(const MessageHeader& header) {
+    std::cout << "msgSize: " << header.getMessageSize() << " msgType: " << header.getMessageType() << " num: " << header.getNumOfFields() << std::endl;
+}
+
+template<typename T>
+void describeGeneratedMsg(const T& msg) {
+    const MessageHeader& header = msg.header_.header;
+    describeMessageHeader(header);
+    std::cout << "headerSize: " << sizeof(msg.header_) << " headerHeader:" << sizeof(header) << " fieldInfoSize:" << sizeof(MessageFieldInfo) << std::endl;
+    uint16_t numOfFields = header.getNumOfFields();
+    MessageFieldInfo* fieldPos = (MessageFieldInfo*)((char*)&(msg.header_) + sizeof(header));
+    for (uint16_t i=0; i < numOfFields; ++i) {
+        describeFieldInfo(*fieldPos++);
+    }
+}
+
 int main() {
     struct testMsg1 t1;
     struct testMsg2 t2;
-    std::cout << sizeof(t1) << std::endl;
-    std::cout << sizeof(t2) << std::endl;
+    describeGeneratedMsg(t1);
+    describeGeneratedMsg(t2);
 }
