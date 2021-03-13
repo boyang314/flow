c4b7923a48e62dc95c9549747f51e13f662f8381
bliu <boyangliu@yahoo.com>
2021-03-13 13:17:39 -0500

    add self testing code

--- net/epoll/message/messaging/test.C
+++ net/epoll/message/messaging/test.C
@@ -21,9 +21,51 @@ void describeGeneratedMsg(const T& msg) {
     }
 }
 
+template<typename T>
+void process(MessageInputBuffer& buffer) {
+    T tmp(buffer);
+    describeMessageHeader(tmp.header_.header);
+    std::cout << tmp.intField_ << ':' << tmp.doubleField_ << '\n';
+}
+
+template<>
+void process<testMsg2>(MessageInputBuffer& buffer) {
+    testMsg2 tmp(buffer);
+    std::cout << tmp.intField_ << ':' << tmp.doubleField_ << ':' << tmp.stringField_ << ':' << tmp.stringField2_ << '\n';
+}
+
+void processMessageInputBuffer(MessageInputBuffer& buffer) {
+    auto msgType = buffer.getMessageType();
+    //std::cout << msgType << std::endl;
+    switch(msgType) {
+        case testMessageTypeIdsEnum::testMsg1: process<testMsg1>(buffer); break;
+        case testMessageTypeIdsEnum::testMsg2: process<testMsg2>(buffer); break;
+        default: std::cout << "unrecognized message type: " << msgType << '\n'; break;
+    }
+}
+
+void processBuffer(const char* buffer, const uint32_t len) {
+    if (len > sizeof(MessageHeader)) {
+        MessageHeader* header = (MessageHeader*)buffer;
+        //describeMessageHeader(*header);
+        if (len == header->getMessageSize()) {
+            MessageInputBuffer input(buffer, len);
+            processMessageInputBuffer(input);
+        }
+    }
+}
+
+
 int main() {
-    struct testMsg1 t1;
-    struct testMsg2 t2;
+    struct testMsg1 t1(3, 5.1);
+    struct testMsg2 t2(9, 6.3, "hello", "worldworldworldworldworldworld");
     describeGeneratedMsg(t1);
     describeGeneratedMsg(t2);
+    processBuffer((const char*)&t1, sizeof(t1));
+    processBuffer((const char*)&t2, sizeof(t2));
+    /*
+    GenericMessage msg;
+    t1.toGenericMessage(msg);
+    std::cout << msg.getMessageType() << std::endl;
+    */
 }
