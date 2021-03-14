fb4308e9ee3b75914a094c33012295bd0b81877c
bliu <boyangliu@yahoo.com>
2021-03-13 20:53:48 -0500

    more tests

---
+++ net/epoll/tests/build.sh
@@ -0,0 +1,1 @@
+g++ -g -o test *.C messaging/*.C -lpthread
---
+++ net/epoll/tests/testMessaging.C
@@ -0,0 +1,52 @@
+#include "epollservice.H"
+
+#include <iostream>
+#include <unistd.h>
+
+#include "MessageHandler.H"
+
+//StreamListener listener;
+MessageHandler listener;
+
+int main() {
+
+    EpollActiveObject service("test");
+
+    TcpConnectionServer* tcpServer = service.createTcpConnectionServer(4567, &listener);
+    if (!tcpServer) exit(1);
+
+    UdpUnicast* udpUnicast = service.createUdpUnicast("udp:/eth0/localhost/5678");
+    if (!udpUnicast) exit(1);
+
+    McastSender* mcastSender = service.createMcastSender("udp:/eth0/239.1.1.1/6789");
+    if (!mcastSender) exit(1);
+
+    McastReceiver* mcastReceiver = service.createMcastReceiver("udp:/eth0/239.1.1.1/6789");
+    if (!mcastReceiver) exit(1);
+
+    service.start();
+
+    //sleep(1);
+    TcpConnection* tcpClient = service.createTcpConnection("tcp:/localhost/4567", &listener);
+    //TcpConnection* tcpClient = service.createTcpConnection("tcp:/127.0.0.1/4567", &listener);
+    if (!tcpClient) exit(1);
+
+    const char tcpMsg[] = "tcp::message";
+    const char udpMsg[] = "udp::message";
+    const char mcastMsg[] = "mcast::message";
+
+    testMsg1 tMsg(3, 5.1);
+
+    uint64_t counter = 0;
+    while(service.isRunning()) {
+        switch(counter % 3) {
+        //case 0: tcpClient->send(tcpMsg, sizeof(tcpMsg)); break;
+        case 0: tcpClient->send((char*)(&tMsg), sizeof(tMsg)); break;
+        case 1: udpUnicast->send(udpMsg, sizeof(udpMsg)); break;
+        case 2: mcastSender->send(mcastMsg, sizeof(mcastMsg)); break;
+        }
+        ++counter;
+        if (counter > 60) service.stop();
+        sleep(1);
+    }
+}
