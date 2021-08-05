e6a7044be41ede05b242e077d92ed3df2327be78
boyang314 <boyangliu@yahoo.com>
2021-08-05 07:35:41 -0400

    add comment

--- net/test/build.sh
+++ net/test/build.sh
@@ -1,3 +1,6 @@
+#serverr: thread per conn server
+#server2: select based thread per conn
+#server3: select based thread poll
 gcc -o client client.c
 gcc -o server server.c -lpthread
 gcc -o server2 server2.c -lpthread
