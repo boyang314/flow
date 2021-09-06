b0b4c30bc93c39e5f4c8fed9b7eaf2a856856a36
boyang314 <boyangliu@yahoo.com>
2021-09-06 16:45:37 -0400

    test memfd

--- sync/tmemfd.c
+++ sync/tmemfd.c
@@ -6,16 +6,33 @@
 #include <stdio.h>
 #include <sys/syscall.h>
 #include <sys/mman.h>
+#include <thread>
 
 #define errExit(msg)    do { perror(msg); exit(EXIT_FAILURE); \
 } while (0)
 
-#define memfd_create(name) syscall(__NR_memfd_create, name, 0);
+#define memfd_create(name) syscall(__NR_memfd_create, name, 0)
+#define gettid() syscall(SYS_gettid)
+
+void peek(int& fd) {
+    off_t oft = 0;
+    const size_t sz = 13;
+    char buf[sz];
+    for (int i=0; i<1000000; ++i) {
+        //bzero(buf, sz);
+        //lseek(fd, oft, SEEK_SET);
+        //int ret = read(fd, buf, sizeof(buf));
+        int ret = pread(fd, buf, sizeof(buf), oft);
+        if (ret == -1) errExit("read");
+        if ((i+1)%100000==0) printf("%ld:%d:%s\n", gettid(), i, buf);
+        //sleep(1);
+        oft += sz;
+    }
+}
 
 int main(int argc, char *argv[])
 {
     const char *name = "";
-    //char *name = "/tmp/t.t";
     ssize_t len = 1024UL*1024*1024*5;
 
     //fd = memfd_create(name, MFD_ALLOW_SEALING);
@@ -30,10 +47,15 @@ int main(int argc, char *argv[])
     for (ssize_t i=0; i<1000000; ++i) 
         write(fd, hello, sizeof(hello));
 
+    std::thread t1([&fd] { peek(fd); });
+    std::thread t2([&fd] { peek(fd); });
+
     /* Keep running, so that the file created by memfd_create()
        continues to exist */
-    pause();
+    //pause();
 
+    t1.join();
+    t2.join();
     exit(EXIT_SUCCESS);
 }
 
